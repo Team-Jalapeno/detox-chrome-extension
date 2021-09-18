ae12ec3115ad7be236d2460aa72ff79c1f4d20f1
thebongy <rishit.bansal0@gmail.com>
2021-09-18 22:20:08 +0530

    feat: Add basic working nsfw filter

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,14 @@
+import { nanoid } from 'nanoid';
+
 function polling() {
   // console.log("polling");
   setTimeout(polling, 1000 * 30);
 }
 
 polling();
-const detoxReport = function (selection: any) {
+const detoxReport = () => {
   chrome.runtime.sendMessage('hello');
-  console.log(`selected`);
+  console.log('selected');
   // $(window).mouseenter(function (event) {
   //   $(event.target).addClass("el-selection");
   // });
@@ -22,7 +24,34 @@ const detoxReport = function (selection: any) {
 };
 
 chrome.contextMenus.create({
-  title: "Report to Detox",
-  contexts: ["all"],
+  title: 'Report to Detox',
+  contexts: ['all'],
   onclick: detoxReport,
+});
+
+const uid = nanoid(16);
+chrome.storage.sync.get(['userid', 'level', 'text', 'videos', 'images'], (items) => {
+  console.log(items);
+  if (items.userid === undefined) {
+    chrome.storage.sync.set({ userid: uid });
+    console.log('generated new user id');
+  }
+
+  if (items.level === undefined) {
+    chrome.storage.sync.set({ level: 66.666 });
+  }
+
+  if (items.text === undefined) {
+    chrome.storage.sync.set({ text: true });
+  }
+
+  if (items.videos === undefined) {
+    chrome.storage.sync.set({ videos: true });
+  }
+
+  if (items.images === undefined) {
+    chrome.storage.sync.set({ images: true });
+  }
+
+  console.log('read from config');
 });
