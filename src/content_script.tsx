f358f48b44f7cb2aba24348d967fb6a29f4c25a3
Rohan Mukherjee <roerohan@gmail.com>
2021-09-19 01:36:34 +0530

    Dev (#1)
    * feat: community reporting
    
    * [WIP] merge conflicts
    
    * fix: merge conflicts
    
    Co-authored-by: Subham-Panda <im.sspanda2001@gmail.com>

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -2,9 +2,85 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable guard-for-in */
 /* eslint-disable no-await-in-loop */
+/* eslint-disable no-undef */
 
 import { FilterAllImagesOnPage, UnfilterAllImagesOnPage } from './filters/nsfw';
 import { getConfig } from './util/config';
+
+function getCSSPath(el: any, callback: any) {
+  let fullPath = '';
+  // eslint-disable-next-line no-shadow
+  const cssPathFn = (el: any, callback: any) => {
+    let elPath = '';
+
+    elPath = $(el).prop('tagName').toLowerCase();
+
+    if (typeof $(el).attr('id') !== 'undefined') {
+      elPath = `${elPath}#${$(el).attr('id')}`;
+    }
+
+    if (typeof $(el).attr('class') !== 'undefined') {
+      elPath = `${elPath}.${$(el).attr('class')!.split(' ').join('.')}`;
+    }
+
+    fullPath = `${elPath} ${fullPath}`;
+
+    if (typeof $(el).parent().prop('tagName') !== 'undefined') {
+      cssPathFn($(el).parent(), callback);
+    } else {
+      callback(fullPath);
+    }
+  };
+
+  cssPathFn(el, callback);
+}
+
+const startOverlay = () => {
+  $('*').not('body, html').hover(function (e) {
+    $(this).css('border', '1px solid #000');
+    e.stopPropagation();
+  }, function (e) {
+    $(this).css('border', '0px');
+    e.stopPropagation();
+  });
+
+  $(window).click((event) => {
+    $('*').not('body,html').hover(function () {
+      $(this).css('border', 'none');
+    });
+    $(window).off('click');
+    getCSSPath(event.target, (path: string) => {
+      console.log(path);
+
+      $.ajax({
+        type: 'POST',
+
+        url: '', // server URL (with https)
+        data: {
+          userid: '',
+          csspath: path,
+          url: window.location.href,
+        },
+        success() {
+          alert('successfully reported');
+        },
+      });
+    });
+
+    return false;
+  });
+};
+
+chrome.runtime.onMessage.addListener((msg) => {
+  console.log(msg);
+  switch (msg.data) {
+    case 'start-overlay':
+      startOverlay();
+      break;
+    default:
+      break;
+  }
+});
 
 chrome.storage.onChanged.addListener(async (changes) => {
   const config = await getConfig();
@@ -31,10 +107,3 @@ window.onload = async () => {
     FilterAllImagesOnPage(config.level);
   }
 };
-
-function nodeInsertedCallback() {
-  // Node inserted callback
-  // console.log(event.target.querySelectorAll('img'));
-}
-
-document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
