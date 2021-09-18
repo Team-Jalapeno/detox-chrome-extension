831c20cc8d227e3ffaa3e25512b38fe4498e8330
roerohan <roerohan@gmail.com>
2021-09-18 18:10:59 +0530

    feat: Basic layout

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,57 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Slider,
+  Typography,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  Switch,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    width: 326,
+    height: 414,
+  },
+});
 
 const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
+  const classes = useStyles();
+  const [sliderValue, setSliderValue] = useState<number | number[]>(50);
+  const [filters, setFilters] = useState<{
+    text: boolean,
+    images: boolean,
+    videos: boolean,
+  }>({
+    text: false,
+    images: false,
+    videos: false,
+  });
 
-  useEffect(() => {
-    chrome.browserAction.setBadgeText({ text: count.toString() });
-  }, [count]);
+  // useEffect(() => {
+  //   chrome.browserAction.setBadgeText({ text: count.toString() });
+  // }, [count]);
 
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
+  // useEffect(() => {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //     setCurrentURL(tabs[0].url);
+  //   });
+  // }, []);
 
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: '#555555',
-          },
-          (msg) => {
-            console.log('result message:', msg);
-          },
-        );
-      }
-    });
+  const sliderOnChange = (event: object, value: number | number[]) => {
+    console.log(event, value);
+    setSliderValue(value);
+  };
+
+  const filtersOnChange = (event: { target: { name: string, checked: boolean } }) => {
+    setFilters({ ...filters, [event.target.name]: event.target.checked });
   };
 
   return (
-    <>
-      <ul style={{ minWidth: '700px' }}>
-        <li>
-          Current URL:
-          {currentURL}
-        </li>
-        <li>
-          Current Time:
-          {new Date().toLocaleTimeString()}
-        </li>
-      </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: '5px' }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
-    </>
+    <div className={classes.root}>
+      <Typography gutterBottom>
+        What do you want to hide?
+      </Typography>
+      <Slider value={sliderValue} onChange={sliderOnChange} aria-labelledby="continuous-slider" />
+      <Typography gutterBottom>
+        What do you want to filter?
+      </Typography>
+
+      <FormControl component="fieldset">
+        <FormGroup>
+          <FormControlLabel
+            control={<Switch checked={filters.text} onChange={filtersOnChange} name="text" />}
+            label="Text"
+          />
+          <FormControlLabel
+            control={<Switch checked={filters.images} onChange={filtersOnChange} name="images" />}
+            label="Images"
+          />
+          <FormControlLabel
+            control={<Switch checked={filters.videos} onChange={filtersOnChange} name="videos" />}
+            label="Videos"
+          />
+        </FormGroup>
+        <FormHelperText>Built with ♥ by Team</FormHelperText>
+      </FormControl>
+    </div>
   );
 };
 
