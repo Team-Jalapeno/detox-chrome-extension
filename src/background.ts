55b074ba8e7f304873b876b43cb58dbfa0701d0d
Subham-Panda <im.sspanda10@gmail.com>
2021-09-18 19:47:23 +0530

    [WIP] context menu message passing

--- src/background.ts
+++ src/background.ts
@@ -4,3 +4,25 @@ function polling() {
 }
 
 polling();
+const detoxReport = function (selection: any) {
+  chrome.runtime.sendMessage('hello');
+  console.log(`selected`);
+  // $(window).mouseenter(function (event) {
+  //   $(event.target).addClass("el-selection");
+  // });
+
+  // $(window).mouseleave(function (event) {
+  //   $(event.target).removeClass("el-selection");
+  // });
+
+  // $(window).click(function (event) {
+  //   console.log("selected: ", event.target);
+  //   return false;
+  // });
+};
+
+chrome.contextMenus.create({
+  title: "Report to Detox",
+  contexts: ["all"],
+  onclick: detoxReport,
+});
