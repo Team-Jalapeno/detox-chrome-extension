5df41786e8e18cf50f2dd462b260751a32427b76
roerohan <roerohan@gmail.com>
2021-09-18 18:12:38 +0530

    feat: Styled the buttons

--- src/popup.tsx
+++ src/popup.tsx
@@ -9,7 +9,7 @@ import {
   FormHelperText,
   Switch,
 } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
@@ -17,6 +17,58 @@ const useStyles = makeStyles({
     height: 414,
   },
 });
+
+const StyledSwitch = withStyles((theme) => ({
+  root: {
+    width: 42,
+    height: 26,
+    padding: 0,
+    margin: theme.spacing(1),
+  },
+  switchBase: {
+    padding: 1,
+    '&$checked': {
+      transform: 'translateX(16px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        backgroundColor: '#52d869',
+        opacity: 1,
+        border: 'none',
+      },
+    },
+    '&$focusVisible $thumb': {
+      color: '#52d869',
+      border: '6px solid #fff',
+    },
+  },
+  thumb: {
+    width: 24,
+    height: 24,
+  },
+  track: {
+    borderRadius: 26 / 2,
+    border: `1px solid ${theme.palette.grey[400]}`,
+    backgroundColor: theme.palette.grey[50],
+    opacity: 1,
+    transition: theme.transitions.create(['background-color', 'border']),
+  },
+  checked: {},
+  focusVisible: {},
+}))(({ classes, ...props }: any) => (
+  <Switch
+    focusVisibleClassName={classes.focusVisible}
+    disableRipple
+    classes={{
+      root: classes.root,
+      switchBase: classes.switchBase,
+      thumb: classes.thumb,
+      track: classes.track,
+      checked: classes.checked,
+    }}
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...props}
+  />
+));
 
 const Popup = () => {
   const classes = useStyles();
@@ -42,7 +94,6 @@ const Popup = () => {
   // }, []);
 
   const sliderOnChange = (event: object, value: number | number[]) => {
-    console.log(event, value);
     setSliderValue(value);
   };
 
@@ -63,15 +114,15 @@ const Popup = () => {
       <FormControl component="fieldset">
         <FormGroup>
           <FormControlLabel
-            control={<Switch checked={filters.text} onChange={filtersOnChange} name="text" />}
+            control={<StyledSwitch checked={filters.text} onChange={filtersOnChange} name="text" />}
             label="Text"
           />
           <FormControlLabel
-            control={<Switch checked={filters.images} onChange={filtersOnChange} name="images" />}
+            control={<StyledSwitch checked={filters.images} onChange={filtersOnChange} name="images" />}
             label="Images"
           />
           <FormControlLabel
-            control={<Switch checked={filters.videos} onChange={filtersOnChange} name="videos" />}
+            control={<StyledSwitch checked={filters.videos} onChange={filtersOnChange} name="videos" />}
             label="Videos"
           />
         </FormGroup>
